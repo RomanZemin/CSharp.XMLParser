0d1f93df222d9895f3a24fdbab5bc7b38089435d
RomanZemin <RomanZemin12@yandex.ru>
2024-09-17 14:42:53 +0700

    adding new files

---
+++ CST.Domain/Entities/Order.cs
@@ -0,0 +1,13 @@
+﻿namespace CST.Domain.Entities
+{
+    public class Order
+    {
+        public int OrderID { get; set; }
+        public int UserID { get; set; }
+        public decimal TotalPrice { get; set; }
+        public DateTime OrderDate { get; set; }
+        public string? Status { get; set; }
+        public User? User { get; set; }
+        public List<OrderItem>? OrderItems { get; set; }
+    }
+}
